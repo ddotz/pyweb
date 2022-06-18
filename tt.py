af9b666a2f9c4d0e43598a185e6f998ef06fd39a
dotzhang <zhfunn@gmail.com>
2022-06-18 20:16:53 +0800

    change dir

---
+++ tt.py
@@ -0,0 +1,7 @@
+class Hello:
+    def __init__(self):
+        pass
+    def p(self):
+        print('hello')
+h = Hello()
+print(h)
--- webtest/tt.py
+++
@@ -1,7 +0,0 @@
-class Hello:
-    def __init__(self):
-        pass
-    def p(self):
-        print('hello')
-h = Hello()
-print(h)
