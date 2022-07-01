bddd133091d9468610c3fa385b0555b8acb30147
dotzhang <zhfunn@gmail.com>
2022-07-01 17:57:24 +0800

    modify run.py

--- run.py
+++ run.py
@@ -4,14 +4,16 @@ from routes.main import main
 from routes.book import book
 import models.book, models.user
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1/book'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.register_blueprint(main)
+app.register_blueprint(book, url_prefix='/book')
+app.secret_key = 'zhang'
+db.init_app(app)
+
+
 if __name__ =='__main__':
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1/book'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.register_blueprint(main)
-    app.register_blueprint(book, url_prefix='/book')
-    app.secret_key = 'zhang'
-    db.init_app(app)
     # db.create_all(app=app)
     #from gevent import pywsgi
     # server = pywsgi.WSGIServer(('0.0.0.0',5001),app)
